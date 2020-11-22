bf33ea9b9757d06af54abc410f00d4654822e7ff
soyoung <soyoungp02@gmail.com>
2020-11-22 23:49:28 +0900

    feat: Auth 기능 만들기

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const { User } = require('../models/User')
+
+let auth = (req, res, next) => {
+  // 클라이언트 쿠키에서 토큰을 가져온다.
+  let token = req.cookie.x_auth
+  // 토큰을 복호화 한 후 유저를 찾는다.
+  User.findByToken(token, (err, user) => {
+    if (err) throw err;
+
+    // 유저가 없으면 인증 No!
+    if (!user) return res.json({ success: false, error: err })
+    
+    // 유저가 있으면 인증 Ok!
+    req.token = token;
+    req.user = user;
+    next()
+  })
+}
+
+module.exports = { auth }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const saltRounds = 10
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -43,6 +44,34 @@ userSchema.pre('save', function (next) {
     next()
   }
 })
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err)
+    return cb(null, isMatch)
+  })
+}
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  const token = jwt.sign(user._id.toHexString(), 'secretToken')
+  user.token = token
+  user.save(function (err, user) {
+    if (err) return cb(err)
+    cb(null, user)
+  })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+  // 토큰을 decode 한다.
+  // 토큰은 user._id + 'secretToken' = token 이기 때문에 token - 'secretToken' => id 구할 수 있다.
+  jwt.verify(token, 'secretToken', function (err, decoded) {
+    user.findOne({ _id: decoded, token }, function (err, user) {
+      if (err) return cb(err)
+      cb(null, user)
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
