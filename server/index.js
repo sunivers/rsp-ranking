04b05b95a891d793e2fce01efb67fa98f0dfda41
soyoung park <soyoungp02@gmail.com>
2020-11-27 23:51:32 +0900

    📦 Add and Set axios and proxy middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -26,6 +25,10 @@ mongoose
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hello~~!!');
 });
 
 app.post('/api/users/register', (req, res) => {
@@ -84,6 +87,7 @@ app.get('/api/users/logout', auth, (req, res) => {
   });
 });
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
