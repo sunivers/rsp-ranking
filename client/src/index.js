0ac371429f309d9c331452e14f26847239b45d87
soyoung park <soyoungp02@gmail.com>
2020-11-28 14:33:34 +0900

    🐛 Fix build error

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
-import { applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
