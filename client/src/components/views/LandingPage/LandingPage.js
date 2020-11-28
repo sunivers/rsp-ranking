51190a647ef9736fb66ea6f84177a77bd5115bf3
soyoung park <soyoungp02@gmail.com>
2020-11-28 23:13:47 +0900

    ✨ Complete logout feature

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,30 +1,34 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((res) => console.log(res));
   });
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((res) => {
+      console.log(res);
+      if (res.data.success) {
+        props.history.push('/login');
+      } else {
+        alert('로그아웃에 실패했습니다.');
+      }
+    });
+  };
 
   return (
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
-        alignContent: 'center',
+        alignItems: 'center',
         width: '100%',
         height: '100vh',
       }}
     >
-      <h2
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
-      >
-        시작 페이지
-      </h2>
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
