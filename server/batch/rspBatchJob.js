990c2b6b8f54bb3aa87b1c5899c15b5e67e793ed
soyoung <soyoungp02@gmail.com>
2020-12-06 00:47:21 +0900

    ✨ 배치 봇 저장시 히스토리 업데이트 쿼리 추가

--- server/batch/rspBatchJob.js
+++ server/batch/rspBatchJob.js
@@ -1,5 +1,9 @@
 const schedule = require('node-schedule');
 const { Bot } = require('../models/Bot');
+const { History } = require('../models/History');
+
+const WIN = 'WIN';
+const LOSE = 'LOSE';
 
 function rspBatchJob() {
   schedule.scheduleJob('0 * * * * *', function () {
@@ -12,6 +16,34 @@ function rspBatchJob() {
     bot.save((err, botInfo) => {
       if (err) return console.error('Bot save failed');
       return console.log('bot save success');
+    });
+
+    const mapRsp = [
+      {
+        [WIN]: 1,
+        [LOSE]: 2
+      },
+      {
+        [WIN]: 2,
+        [LOSE]: 0
+      },
+      {
+        [WIN]: 0,
+        [LOSE]: 1
+      }
+    ]
+
+    History.updateMany({ hour: 0, rsp: mapRsp[rsp].WIN }, { $set: { state: 'WIN', point: 7 } }, (err, output) => {
+      if (err) {
+        console.log('Update error', err);
+      }
+      console.log('Update success', output);
+    });
+    History.updateMany({ hour: 0, rsp: mapRsp[rsp].LOSE }, { $set: { state: 'LOSE', point: -3 } }, (err, output) => {
+      if (err) {
+        console.log('Update error', err);
+      }
+      console.log('Update success', output);
     });
   });
 }
