323b84c677e775b72d7c5e23f1c41335c2a2cb46
soyoung <soyoungp02@gmail.com>
2020-12-12 00:50:51 +0900

    🐛 Fix error about router

--- client/src/components/views/MyPage.js
+++ client/src/components/views/MyPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 function MyPage(props) {
   const onClickHandler = () => {
@@ -19,4 +20,4 @@ function MyPage(props) {
   );
 }
 
-export default MyPage;
+export default withRouter(MyPage);
