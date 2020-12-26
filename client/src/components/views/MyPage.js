3f54119da55596f0631b437d9a25fbae44c988a2
soyoung <soyoungp02@gmail.com>
2020-12-27 00:33:10 +0900

    ✨ 마이페이지 참여기록 화면 작업

--- client/src/components/views/MyPage.js
+++ client/src/components/views/MyPage.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-import { Menu } from 'antd';
+import { Menu, Table } from 'antd';
 import { BookOutlined, PoweroffOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 const rootSubmenuKeys = ['history', 'logout'];
 
 function MyPage(props) {
-  const [openKeys, setOpenKeys] = React.useState(['']);
+  const [openKeys, setOpenKeys] = useState(['']);
+  const [history, setHistory] = useState([]);
+
+  async function fetchData() {
+    const res = await axios.get('/api/users/history');
+    setHistory(res.data.history);
+  }
 
   const onOpenChange = keys => {
     const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
@@ -16,6 +22,9 @@ function MyPage(props) {
       setOpenKeys(keys);
     } else {
       setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+      if (latestOpenKey === 'history' && !history.length) {
+        fetchData();
+      }
     }
   };
 
@@ -29,14 +38,42 @@ function MyPage(props) {
     });
   };
 
+  const columns = [
+    {
+      title: '날짜',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: '시간',
+      dataIndex: 'hour',
+      key: 'hour',
+    },
+    {
+      title: '참여 종류',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: '승/패',
+      dataIndex: 'state',
+      key: 'state',
+    },
+    {
+      title: '증감 포인트',
+      dataIndex: 'point',
+      key: 'point',
+    },
+  ];
+  
+  const rsp = ['가위', '바위', '보'];
+  const data = history.map(h => ({...h, type: rsp[h.rsp]}));
+
   return (
     <div>
       <Menu mode="inline" openKeys={openKeys} onOpenChange={onOpenChange} style={{ border: 'none' }}>
         <SubMenu key="history" icon={<BookOutlined />} title="내 참여 기록">
-          {/* <Menu.Item key="1">Option 1</Menu.Item>
-          <Menu.Item key="2">Option 2</Menu.Item>
-          <Menu.Item key="3">Option 3</Menu.Item>
-          <Menu.Item key="4">Option 4</Menu.Item> */}
+          <Table columns={columns} dataSource={data} />
         </SubMenu>
         <Menu.Item key="logout" icon={<PoweroffOutlined />} onClick={onClickHandler}>
           로그아웃
